3aa728735eb49953989b310248e780104d930ee3
Joe Zane <joezane.cn@gmail.com>
2023-02-20 20:56:58 +0800

    1. update demo of image process `canny`

--- src/image_canny.rs
+++ src/image_canny.rs
@@ -1,22 +1,21 @@
 #![allow(unused_imports)]
 #![allow(dead_code)]
+use crate::opencv_util::imshow_many;
 use opencv::{
     core::{Size, BORDER_DEFAULT},
-    imgcodecs::{imread, IMREAD_COLOR},
-    imgproc::gaussian_blur,
+    imgcodecs::{imread, IMREAD_COLOR, IMREAD_GRAYSCALE},
+    imgproc::{canny, gaussian_blur},
     prelude::*,
 };
-use crate::opencv_util::imshow_many;
 
 // Canny 边缘检测
 pub fn image_canny() {
-    let src = imread("young.jpg", IMREAD_COLOR).unwrap();
+    let src = imread("young.jpg", IMREAD_GRAYSCALE).unwrap();
 
-    imshow_many("Canny", &[&src], false)
-}
+    let mut c1 = Mat::default();
+    canny(&src, &mut c1, 80., 150., 3, false).unwrap();
+    let mut c2 = Mat::default();
+    canny(&src, &mut c2, 50., 100., 3, false).unwrap();
 
-fn image_gaussian_filter(mat: &Mat) -> Mat {
-    let mut gaussian = Mat::default();
-    gaussian_blur(&mat, &mut gaussian, Size::new(3, 3), 1., 0., BORDER_DEFAULT).unwrap();
-    gaussian
+    imshow_many("Canny", &[&src, &c1, &c2], true)
 }
